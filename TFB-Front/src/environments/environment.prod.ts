e754efe81159c27b3a12d72cf87498b0df64b87b
luxury tech <luxurytech30@gmail.com>
2026-06-26 16:08:20 +0300

    b

--- TFB-Front/src/environments/environment.prod.ts
+++ TFB-Front/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tfb-ydjk.onrender.com',
+  apiUrl: 'https://tfb-s7lb.onrender.com',
+  // apiUrl: 'https://tfb-ydjk.onrender.com',
 };
